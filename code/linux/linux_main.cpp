190d18404925ac156749b4a2d20d1f4db12a0e0e
xLAva <jochen.leopold@model-view.com>
2013-05-17 18:55:27 +0200

    AssetLoading: sort files but ignore the case (fixes some mods)

--- code/linux/linux_main.cpp
+++ code/linux/linux_main.cpp
@@ -24,6 +24,7 @@
 #include <stdarg.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <algorithm>
 
 #include <dlfcn.h>
 
@@ -238,7 +239,20 @@ DIRECTORY SCANNING
 
 ==============================================================
 */
-#define	MAX_FOUND_FILES	0x1000
+
+
+int alphasortIgnoreCase(const struct dirent ** a, const struct dirent **b)
+{
+	std::string aName = (*a)->d_name;
+	std::string bName = (*b)->d_name;
+	
+	std::transform(aName.begin(), aName.end(), aName.begin(), ::tolower);
+	std::transform(bName.begin(), bName.end(), bName.begin(), ::tolower);
+	
+	return aName.compare(bName);
+	
+}
+
 
 #define	MAX_FOUND_FILES	0x1000
 
@@ -273,7 +287,7 @@ char **Sys_ListFiles( const char *directory, const char *extension, int *numfile
 	// search
 	nfiles = 0;
 
-    scancount = scandir(directory, &scanlist, NULL, alphasort);
+    scancount = scandir(directory, &scanlist, NULL, alphasortIgnoreCase);
     if (scancount < 0) {
 		*numfiles = 0;
 		return NULL;
